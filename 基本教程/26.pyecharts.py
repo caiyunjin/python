df6a52afadf877f3a080435af7cc25c7d7ac1087
caiyunjin <caiyunjin@vip.qq.com>
2023-04-24 07:22:05 +0800

    1

--- 基本教程/26.pyecharts.py
+++ 基本教程/26.pyecharts.py
@@ -7,7 +7,7 @@ FilePath: \python\基本教程\26.pyecharts.py
 Description: 
 '''
 from pyecharts.charts import Line
-from pyecharts.options import TitleOpts,LegendOpts,ToolboxOpts,VisualMapOpts
+from pyecharts.options import TitleOpts, LegendOpts, ToolboxOpts, VisualMapOpts
 
 # 创建一个折线图对象
 line = Line()
@@ -17,7 +17,7 @@ line.add_xaxis(['中国', '美国', '韩国'])
 line.add_yaxis('GDP', [120, 132, 101])
 # 标题设置全局配置项
 line.set_global_opts(
-    title_opts=TitleOpts(title='GDP展示'pos_left='center',pos_bottom='1%'),
+    title_opts=TitleOpts(title='GDP展示', pos_left='center', pos_bottom='1%'),
     legend_opts=LegendOpts(is_show=True),
     toolbox_opts=ToolboxOpts(is_show=True),
     visualmap_opts=VisualMapOpts(is_show=True),
